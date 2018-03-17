460a32198cad91b66a2bb4d329d8fab3d2fb8d4e
Sylphy0052 <ma17099@shibaura-it.ac.jp>
2018-03-17 23:53:15 +0900

    rotate complete

--- CameraTest/Assets/Scripts/CameraManager.cs
+++ CameraTest/Assets/Scripts/CameraManager.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour {
+	private Quaternion firstCameraQuaternion;
 
 	// Use this for initialization
 	void Start () {
 		Input.gyro.enabled = true;
+		firstCameraQuaternion = this.transform.rotation;
+//		Quaternion gyroFirst = Input.gyro.attitude;
+//		firstCameraQuaternion = Quaternion.Euler(90, 0, 0) * (new Quaternion(-gyroFirst.x, -gyroFirst.y, gyroFirst.z, gyroFirst.w));
+//		Debug.Log (firstCameraQuaternion);
 	}
 
 	// Update is called once per frame
@@ -26,6 +31,17 @@ public class CameraManager : MonoBehaviour {
 
 	void RotateByGyro() {
 		Quaternion gyro = Input.gyro.attitude;
-		this.transform.localRotation = Quaternion.Euler(90, 0, 0) * (new Quaternion(-gyro.x, -gyro.y, gyro.z, gyro.w));
+		Quaternion gyroQuaternion = Quaternion.Euler(90, 0, 0) * (new Quaternion(-gyro.x, -gyro.y, gyro.z, gyro.w));
+//		Debug.Log (gyroQuaternion);
+//		Vector3 gyroVector3 = gyroQuaternion.eulerAngles;
+//		Debug.Log (gyroVector3);
+//		this.transform.Rotate (0, gyroVector3.y, 0);
+//		this.transform.rotation = gyroQuaternion;
+		// Xが縦(上下)->不必要
+		// Yが横(これが必要)これだけだと180度しかいかない
+		// Zが…?
+		// Wが…
+//		this.transform.rotation = new Quaternion(firstCameraQuaternion.x, gyroQuaternion.y, firstCameraQuaternion.z, firstCameraQuaternion.w);
+		this.transform.rotation = new Quaternion(firstCameraQuaternion.x, gyroQuaternion.y, firstCameraQuaternion.z, gyroQuaternion.w);
 	}
 }
