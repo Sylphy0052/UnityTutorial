ae1c4205cd8a5cc3e526747246396733f743bf87
Sylphy0052 <ma17099@shibaura-it.ac.jp>
2018-03-17 23:22:32 +0900

    add CameraTest

---
+++ CameraTest/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Input.gyro.enabled = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+//		RotateByKey();
+		RotateByGyro();
+	}
+
+	void RotateByKey() {
+		if(Input.GetKey(KeyCode.A)) {
+			this.transform.Rotate(0,-50 * Time.deltaTime,0);
+		}
+		if(Input.GetKey(KeyCode.D)) {
+			this.transform.Rotate(0,50 * Time.deltaTime,0);
+		}
+	}
+
+	void RotateByGyro() {
+		Quaternion gyro = Input.gyro.attitude;
+		this.transform.localRotation = Quaternion.Euler(90, 0, 0) * (new Quaternion(-gyro.x, -gyro.y, gyro.z, gyro.w));
+	}
+}
